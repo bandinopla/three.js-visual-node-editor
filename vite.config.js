0c1f56fa87420005917906ea9e8bb3f77c647d72
bandinopla <pablobandinopla@gmail.com>
2025-03-30 06:48:34 +0000

    on github pages the app will be served from a subdirectory

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         plugins: [], 
+        base: mode=="development"? "./" : "/three.js-visual-node-editor/",
         server: {
           host: '0.0.0.0',
           port: 5173, // Replace with your desired port if needed
