b8ee70b97f2b18d83292d72098d00b372538b3df
bandinopla <pablobandinopla@gmail.com>
2025-03-31 10:15:21 +0000

    add verison in the html se ppl can see the version on the app + bump version

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import packageJson from './package.json'; // Import package.json
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,6 +11,9 @@ export default defineConfig(({ mode }) => {
         server: {
           host: '0.0.0.0',
           port: 5173, // Replace with your desired port if needed
-        }  
+        } ,
+        define: {
+            'import.meta.env.APP_VERSION': JSON.stringify(packageJson.version), // Define the version
+          },
       }
 })
